913c4cdcd344f5006366487ccfe6a853a6510ded
JBaptista <jbaptista.macosso@gmail.com>
2024-12-28 10:57:34 +0100

    Correção de erro CORS

--- src/server.js
+++ src/server.js
@@ -8,12 +8,22 @@ const Rotas = require('./routes');
 //Configuração Inicial.
 const PORT = process.env.PORT || 5000;
 const app = express();
-app.use('*', cors({
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // Permitir apenas 'https://example.com'
+  res.header('Access-Control-Allow-Credentials', 'true'); // Permitir envio de credenciais
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+/*app.use('*', cors({
   //origin: 'https://sistema-transporte-react-js.vercel.app',
   origin: 'http://localhost:3000',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
-}));
+  
+}));*/
 
 //Configuração para permitir o JSON
 app.use(express.json());
