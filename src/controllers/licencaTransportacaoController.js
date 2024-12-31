aa5db92660e201b341bcef8d9094ca8e8795f6f7
JBaptista <jbaptista.macosso@gmail.com>
2024-12-31 20:18:44 +0100

    Update Transporte Licença

--- src/controllers/licencaTransportacaoController.js
+++ src/controllers/licencaTransportacaoController.js
@@ -5,6 +5,11 @@ module.exports = {
     async createLicencaTransportacao(req, res) {
       try {
         const { viaturaId, descricao, observacao, proprietario, dataEmissao, dataVencimento, licencaStatus } = req.body;
+        //Verificação.
+        if (!viaturaId || !descricao || !proprietario || !dataEmissao || !dataVencimento || !licencaStatus) {
+          return res.status(400).json({ error: "Todos os campos são obrigatórios." });
+        };
+
         const licenca = await prisma.licencaTransportacaoViaturas.create({
           data: {
             viaturaId,
@@ -16,21 +21,35 @@ module.exports = {
             licencaStatus,
           },
         });
-        res.status(201).json({ message: "Licença de transportação criada com sucesso",licenca});
+        return res.status(201).json({ message: "Licença de transportação criada com sucesso.", licenca});
       } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "Erro ao criar licença de transportação" });
+        return res.status(500).json({ error: "Erro ao criar licença de transportação." });
       }
     },
   
     // Listar Licenças de Transportação
     async getAllLicencasTransportacao(req, res) {
       try {
-        const licencas = await prisma.licencaTransportacaoViaturas.findMany({ include: { viatura: true } });
-        res.status(200).json({licencas: licencas});
+        const licencas = await prisma.licencaTransportacaoViaturas.findMany({
+          select:{
+            id: true,
+            viaturaId: true,
+            descricao: true,
+            observacao: true,
+            proprietario: true,
+            dataEmissao: true,
+            dataVencimento: true,
+            licencaStatus: true,
+            select:{
+              viaturaMarca: true,
+              viaturaModelo: true,
+              viaturaMatricula: true,
+            },
+          }
+        });
+        return res.status(200).json({licencas: licencas});
       } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "Erro ao buscar licenças de transportação" });
+        return res.status(500).json({ error: "Erro ao buscar licenças de transportação." });
       }
     },
   
@@ -38,15 +57,13 @@ module.exports = {
     async getLicencaTransportacaoById(req, res) {
       try {
         const { id } = req.params;
-        const licenca = await prisma.licencaTransportacaoViaturas.findUnique({
-          where: { id: parseInt(id) },
-          include: { viatura: true },
-        });
-        if (!licenca) return res.status(404).json({ error: "Licença de transportação não encontrada" });
-        res.status(200).json({licenca: licenca});
+        const licenca = await prisma.licencaTransportacaoViaturas.findUnique({ where: { id: parseInt(id) }});
+        if (!licenca){ 
+          return res.status(404).json({ error: "Licença de transportação não encontrada." });
+        }
+       return res.status(200).json({licencas: licenca});
       } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "Erro ao buscar licença de transportação" });
+        return res.status(500).json({ message: "Erro ao buscar licença de transportação."+error });
       }
     },
   
@@ -58,25 +75,25 @@ module.exports = {
 
         //Verificar se existe
         const licencaExiste = await prisma.licencaTransportacaoViaturas.findUnique({ where: { id: parseInt(id) }});
-          if (!licencaExiste) return res.status(404).json({ error: "Licença de transportação não encontrada" });
+        if (!licencaExiste){ 
+          return res.status(404).json({ message: "Licença de transportação não encontrada." });
+        };
         
         // Atualizar licença de transporte
-        const licenca = await prisma.licencaTransportacaoViaturas.update({
-          where: { id: parseInt(id) },
+        const licenca = await prisma.licencaTransportacaoViaturas.update({ where: { id: parseInt(id) },
           data: {
-            viaturaId,
-            descricao,
-            observacao,
-            proprietario,
-            dataEmissao,
-            dataVencimento,
-            licencaStatus,
+            viaturaId: viaturaId,
+            descricao: descricao,
+            observacao: observacao,
+            proprietario: proprietario,
+            dataEmissao: dataEmissao,
+            dataVencimento: dataVencimento,
+            licencaStatus: licencaStatus,
           },
         });
-        res.status(200).json({ message: "Licença de transportação atualizada com sucesso", licenca});
+        return res.status(200).json({ message: "Licença de transportação atualizada com sucesso", licenca});
       } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "Erro ao atualizar licença de transportação" });
+        return res.status(500).json({ message: "Erro ao atualizar licença de transportação."+error });
       }
     },
   
@@ -86,14 +103,15 @@ module.exports = {
         const { id } = req.params;
         //Verificar se existe
         const licencaExiste = await prisma.licencaTransportacaoViaturas.findUnique({ where: { id: parseInt(id) }});
-        if (!licencaExiste) return res.status(404).json({ error: "Licença de transportação não encontrada" });
+        if (!licencaExiste){ 
+          return res.status(404).json({ error: "Licença de transportação não encontrada." });
+        };
 
         // Deletar licença de transporte
         await prisma.licencaTransportacaoViaturas.delete({ where: { id: parseInt(id) } });
-        res.status(200).json({ message: "Licença de transportação deletada com sucesso" });
+        return res.status(200).json({ message: "Licença de transportação deletada com sucesso." });
       } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "Erro ao deletar licença de transportação" });
+        return res.status(500).json({ message: "Erro ao deletar licença de transportação. "+error });
       }
     },
   };
